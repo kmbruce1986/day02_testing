37490daf332e97d41077e320ae311d3f322c104f
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-17 15:16:35 +0100

    tests number to short month name

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -60,3 +60,34 @@ def number_to_full_month_name(month_number)
       return "error"
     end
 end
+
+def number_to_short_month_name(month_number)
+  case month_number
+  when 1
+    return "Jan"
+  when 2
+    return "Feb"
+  when 3
+    return "Mar"
+  when 4
+    return "Apr"
+  when 5
+    return "May"
+  when 6
+    return "Jun"
+  when 7
+    return "Jul"
+  when 8
+    return "Aug"
+  when 9
+    return "Sep"
+  when 10
+    return "Oct"
+  when 11
+    return "Nov"
+  when 12
+    return "Dec"
+  else
+    return "error"
+  end
+end
